baa12627d6ffc3af022a516336bcc47a773bc335
Abohte <adinda.bohte@gmail.com>
2017-12-28 17:16:54 +0100

    Add views for Artist index, show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
 class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show, :edit, :update]
 
   def index
     @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
   end
 
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,15 @@
 class SongsController < ApplicationController
+before_action :set_song, only: [:destroy]
+
+  def destroy
+    @artist = @song.artist
+    @song.destroy
+    flash[:success] = "Song deleted"
+    redirect_to @artist
+  end
+
+  private
+    def set_song
+      @song = Song.find(params[:id])
+    end
 end
