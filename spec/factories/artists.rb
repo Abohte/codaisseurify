965adf1f482feb6b0d5ceda6d2a193fcd3ffa958
Abohte <adinda.bohte@gmail.com>
2017-12-28 12:40:47 +0100

    Add models: Artist, Song

---
+++ spec/factories/artists.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :artist do
+    name "MyString"
+    description "MyText"
+    type ""
+  end
+end
---
+++ spec/factories/songs.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :song do
+    title "MyString"
+    year 1
+    artist nil
+  end
+end
